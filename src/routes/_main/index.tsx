21ebd0c0da2508bb628c6bcf9a8c150876c963cb
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2025-07-09 15:45:32 +0500

    updated dashboard page

--- src/routes/_main/index.tsx
+++ src/routes/_main/index.tsx
@@ -1,10 +1,11 @@
 import PageLayout from "@/layouts/page-layout"
+import DashboardPage from "@/pages/dashboard"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/_main/")({
     component: () => (
         <PageLayout title="Dashboard">
-            <h1 className="text-2xl font-bold">Loglar Bosh Sahifasi</h1>
+            <DashboardPage />
         </PageLayout>
     ),
 })
